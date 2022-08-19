858d7d907ff1aaf3e87fbc4db77d73dd0ff9ae67
Aldawai <boubakaridawai5@mail.com>
2022-08-19 09:59:33 +0100

    add: post

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+const cors = (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+};
 
 let _PORT = 4000;
 
 app.get('/',(req,res)=>{
-    res.send('Le rest API fonctionne correctement !');
+    cors(req,res);
+    res.send('Heroku Rest API !');
+});
+
+app.post('/',(req,res)=>{
+    cors(req,res);
+    res.send(req.body);
+    console.log(req.body);
 });
 
 app.listen(process.env.PORT || _PORT);
