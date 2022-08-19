8865334542cfef3c64ed6be7041820e9bb28e858
Aldawai <boubakaridawai5@mail.com>
2022-08-19 10:34:46 +0100

    add:JSON

--- index.js
+++ index.js
@@ -9,11 +9,41 @@ const cors = (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 };
 
+
+let data = '';
+let apiTest;
+
+async function Lire(){
+	data = '';
+	// Create a readable stream
+	var readerStream = fs.createReadStream('mbusrs.json');
+	// Set the encoding to be utf8.
+	readerStream.setEncoding('UTF8');
+	// Handle stream events --> data, end, and error
+	readerStream.on('data', function(chunk) {
+		data += chunk;
+	});
+	readerStream.on('end',function(){
+		let the_data = JSON.parse(data);
+		for (var i = 0; i < the_data.users.length; i++) {
+			const reponse = the_data.users[i];
+			apiTest = app.get(`/api/test/${i}`, (req, res) => {
+				cors(req, res);
+				res.send(reponse);
+			});
+		}
+		console.log(the_data);
+	});
+	readerStream.on('error', function(err){
+		console.log(err.stack);
+	});
+}
+
 let _PORT = 4000;
 
 app.get('/',(req,res)=>{
     cors(req,res);
-    res.send('Heroku Rest API !');
+    res.send(data);
 });
 
 app.post('/',(req,res)=>{
