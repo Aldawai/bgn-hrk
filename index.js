414351eed2782247192b6600a52083a3b323cbdc
Aldawai <87396502+Aldawai@users.noreply.github.com>
2022-09-12 00:00:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const port = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: false}));
-// app.use(bodyParser.json());
+app.get("/", (req, res) => res.type('html').send(html));
 
-const cors = (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-};
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
-let data = '';
-let apiTest;
-
-async function Lire(){
-	data = '';
-	// Create a readable stream
-	var readerStream = fs.createReadStream('https://github.com/Aldawai/bgn-hrk/blob/main/mbusrs.json');
-	// Set the encoding to be utf8.
-	readerStream.setEncoding('UTF8');
-	// Handle stream events --> data, end, and error
-	readerStream.on('data', function(chunk) {
-		data += chunk;
-	});
-	readerStream.on('end',function(){
-		let the_data = JSON.parse(data);
-		for (var i = 0; i < the_data.users.length; i++) {
-			const reponse = the_data.users[i];
-			apiTest = app.get(`/api/test/${i}`, (req, res) => {
-				cors(req, res);
-				res.send(reponse);
-			});
-		}
-		console.log(the_data);
-	});
-	readerStream.on('error', function(err){
-		console.log(err.stack);
-	});
-}
-
-let _PORT = 4000;
-
-app.get('/',(req,res)=>{
-    cors(req,res);
-    res.send(data);
-});
-
-app.post('/',(req,res)=>{
-    cors(req,res);
-    res.send(req.body);
-    console.log(req.body);
-});
-
-app.listen(process.env.PORT || _PORT);
-
-console.log('Server running at localhost:'+_PORT);
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
